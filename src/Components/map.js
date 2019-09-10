9bde93dd2520a4fd4fcb05191f01b0abf233fa2a
joel <jmacd87@gmail.com>
2019-09-10 16:04:50 -0300

    maps

---
+++ src/Components/map.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+ 
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+ 
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 43.8375,
+      lng: -66.1174
+    },
+    zoom: 14
+  };
+ 
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '500px', width: '100%', paddingtop: '300px'}}>
+        <GoogleMapReact
+
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+          yesIWantToUseGoogleMapApiInternals
+        >
+          <AnyReactComponent
+            lat={43.8375}
+            lng={-66.1174}
+            text="Aerial Ridge"
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+ 
+export default SimpleMap;
